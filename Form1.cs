c99c48bac7157ef469fef0f26b9e3dcc59e1f962
0x53616d <47088044+0x53616d@users.noreply.github.com>
2021-09-15 15:15:30 -0700

    Initial Commit

---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win10LockReplacement
+{
+    public partial class Form1 : Form
+    {
+        public static string Password;
+        static bool autoKill;
+        public Form1()
+        {
+            InitializeComponent();
+            autoKill = false; //Assign this a value because thats a pretty good idea
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+            textBox1.PasswordChar = '\0';
+            }
+            else
+            {
+            textBox1.PasswordChar = '*';
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 Secondform = new Form2();
+            Secondform.Show();
+            autoKill = true;
+            Password = textBox1.Text;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (autoKill == true)
+            {
+                System.Diagnostics.Process cmd = new System.Diagnostics.Process();
+                cmd.StartInfo.FileName = "cmd.exe";
+                cmd.StartInfo.Arguments = "/C taskkill /F /IM explorer.exe && taskkill /F /IM taskmgr.exe && taskkill /F /IM discord.exe && taskkill /F /IM python.exe && taskkill /F /IM chrome.exe && taskkill /F /IM brave.exe && taskkill /F /IM firefox.exe && taskkill /F /IM resmon.exe && taskkill /F /IM regedit.exe"/* Add a few apps that should be unavailable during lockdown mode */;
+                cmd.StartInfo.RedirectStandardInput = true;
+                cmd.StartInfo.RedirectStandardOutput = true;
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.UseShellExecute = false;
+                cmd.Start();
+            }
+            else
+            {
+                this.Show();
+            }
+        }
+
+        public static async void doEndTimer(string sc)
+        {
+            if (sc == "CPG")
+            {
+                autoKill = false;
+                await Task.Delay(250); /* Allow for Form2 visual updates */
+                MessageBox.Show("You will now be logged out, please log in again to start the filesystem.");
+                System.Diagnostics.Process cmd = new System.Diagnostics.Process();
+                cmd.StartInfo.FileName = "cmd.exe";
+                cmd.StartInfo.CreateNoWindow = true;
+                cmd.StartInfo.Arguments = "/C shutdown -l";
+                cmd.Start();
+            }
+            else
+            {
+                //this is really bad, failed attempt to unlock, likely malicious
+                System.Diagnostics.Process cmd = new System.Diagnostics.Process();
+                cmd.StartInfo.FileName = "cmd.exe";
+                cmd.StartInfo.Arguments = "/C shutdown -s -t 0";
+                cmd.Start();
+            }
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win10LockReplacement
+{
+    public partial class Form2 : Form
+    {
+        static int attemptsFailed;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == Form1.Password)
+            {
+                Form1.doEndTimer("CPG");
+                this.Hide();   
+            }
+            else
+            {
+                textBox1.Text = "";
+                attemptsFailed++;
+                if (attemptsFailed >= 3)
+                {
+                    textBox1.ReadOnly = true; /* Disallow more attempts. Leaving this form writeable would allow for the 10 second timer to be reset every time a false entry is attempted */
+                    button1.Enabled = false;
+                    MessageBox.Show("Too many failed attempts, shutting down in 10 seconds...");
+                    await Task.Delay(10000);
+                    System.Diagnostics.Process cmd = new System.Diagnostics.Process();
+                    cmd.StartInfo.FileName = "cmd.exe";
+                    cmd.StartInfo.Arguments = "/C shutdown -s -t 0";
+                    cmd.Start();
+                }
+                else
+                {
+                    MessageBox.Show("Incorrect entry, " + (3 - attemptsFailed) + " attempts remaining.");
+                }
+            }
+        }
+    }
+}
